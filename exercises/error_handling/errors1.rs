eeb986149c44eca1e9818a15eb5a55117b710566
shadowlording <15701216382@163.com>
2025-11-30 13:26:39 +0000

    update

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -14,9 +14,10 @@
 pub fn generate_nametag_text(name: String) -> Option<String> {
     if name.is_empty() {
         // Empty names aren't allowed.
+        Err("`name` was empty; it must be nonempty.".into())
         None
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 
