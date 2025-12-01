abbbce33c72a1608db934598311aaaaa2aff0f6f
shadowlording <15701216382@163.com>
2025-12-01 05:51:45 +0000

    update

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -35,6 +35,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    map.values().filter(|&&v| v == value).count()
     todo!();
 }
 
@@ -54,6 +55,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+      collection.iter().map(|map| count_iterator(map, value)).sum()
     todo!();
 }
 
