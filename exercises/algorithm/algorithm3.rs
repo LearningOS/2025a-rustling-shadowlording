09d207f07c8c6fe352b1520b66afbbafb22ab9e8
shadowlording <15701216382@163.com>
2025-12-15 06:57:02 +0000

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -4,17 +4,15 @@
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
 // I AM NOT DONE
-
-fn sort<T>(array: &mut [T]){
-	fn sort<T: Ord>(array: &mut [T]) {
+fn sort<T: Ord>(array: &mut [T]) {
     if array.len() <= 1 {
         return;
     }
     
     let pivot_index = partition(array);
     let (left, right) = array.split_at_mut(pivot_index);
-    sort(&mut left[..pivot_index]);
-    sort(&mut right[1..]);
+    sort(left); // 左半部分：0..pivot_index
+    sort(&mut right[1..]); // 右半部分：pivot_index+1..end
 }
 
 fn partition<T: Ord>(array: &mut [T]) -> usize {
@@ -32,7 +30,6 @@ fn partition<T: Ord>(array: &mut [T]) -> usize {
     i
 }
 
-}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,13 +40,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
