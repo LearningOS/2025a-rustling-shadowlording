ad9bfab96a99565a55d99813c007e3d934c498f4
shadowlording <15701216382@163.com>
2025-12-15 05:42:39 +0000

    update

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -19,11 +17,13 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        // 测试偶数输入返回true（如2）
+        assert!(is_even(2));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        // 测试奇数输入返回false（如5）
+        assert!(!is_even(5));
     }
 }
