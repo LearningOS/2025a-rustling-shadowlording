612f1d9b5052c63e7b6718bad43b1eec28949642
shadowlording <15701216382@163.com>
2025-12-15 06:58:23 +0000

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,21 +6,19 @@
 
 // I AM NOT DONE
 
-#[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        // Clippy提示：`is_none()`可简化为`matches!`或直接判断，但此处保留逻辑即可
     }
 
     let my_arr = [-1, -2, -3, -4, -5, -6];
-    println!("My array! Here it is: {:?}", my_arr);
+    println!("My array! Here it is: {my_arr:?}");
 
     let my_empty_vec: Vec<i32> = Vec::new();
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    println!("This Vec is empty, see? {my_empty_vec:?}");
 
     let mut value_a = 45;
     let mut value_b = 66;
-    value_a.swap(&mut value_b);
-    println!("value a: {}; value b: {}", value_a, value_b);
+    std::mem::swap(&mut value_a, &mut value_b);
+    println!("value a: {value_a}; value b: {value_b}");
 }
