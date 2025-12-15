a29c84590995231232d7264edd6e724490ed244c
shadowlording <15701216382@163.com>
2025-12-15 05:57:27 +0000

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -21,7 +21,6 @@
 // No hints this time!
 
 // I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -31,12 +30,17 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    // 补全函数签名：输入是 Vec<(String, Command)>，输出是 Vec<String>
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // 补全输出类型：Vec<String>
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let result = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => format!("{}{}", string, "bar".repeat(*n)),
+            };
+            output.push(result);
         }
         output
     }
@@ -44,8 +48,8 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    // 补全导入：从 my_module 导入 transformer
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
