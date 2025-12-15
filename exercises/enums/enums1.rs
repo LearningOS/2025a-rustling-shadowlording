b9c436eae4b3b426071414926a33e76097f4e3ae
shadowlording <15701216382@163.com>
2025-12-15 06:04:06 +0000

    update

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,21 +1,14 @@
-// enums1.rs
-//
-// No hints this time! ;)
-
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
     Quit,          
     Echo(String),  
     Move { x: i32, y: i32 },  
-    ChangeColor(u8, u8, u8),  /
+    ChangeColor(u8, u8, u8),  // 修复：去掉多余的斜杠
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo(String::from("Hello")));  // 补充String参数
+    println!("{:?}", Message::Move { x: 10, y: 20 });        // 补充结构体字段
+    println!("{:?}", Message::ChangeColor(255, 0, 0));       // 补充RGB参数
 }
