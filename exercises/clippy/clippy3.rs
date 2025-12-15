e3be7f4415f1abc5211b2a198fa1541783ca6427
shadowlording <15701216382@163.com>
2025-12-15 06:44:22 +0000

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -10,23 +10,17 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-       
+        // Clippy提示：`is_none()`可简化为`matches!`或直接判断，但此处保留逻辑即可
     }
 
-    let my_arr = &[
-        -1, -2, -3,
-        -4, -5, -6
-    ];
+    let my_arr = [-1, -2, -3, -4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let mut my_vec = vec![1, 2, 3, 4, 5];
-    my_vec.resize(0, 5);
-    let my_empty_vec = my_vec;
+    let my_empty_vec: Vec<i32> = Vec::new();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    std::mem::swap(&mut value_a, &mut value_b);
+    value_a.swap(&mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
